b4d351361c63bb34bc3169ff8e7da73dfe00ffb5
Carlos Sampol <sampol.90@gmail.com>
2018-12-26 23:53:08 -0500

    Updates

---
+++ precache-manifest.211a2f1b6776df7ed31baf63ec501912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7595909ac93cd755b8de497e42bdbbcf",
+    "url": "./static/media/single-logo.7595909a.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "89d869d4d4a61e7e2913",
+    "url": "./static/js/main.89d869d4.chunk.js"
+  },
+  {
+    "revision": "bb8469d79c342f425900",
+    "url": "./static/js/1.bb8469d7.chunk.js"
+  },
+  {
+    "revision": "89d869d4d4a61e7e2913",
+    "url": "./static/css/main.12697db0.chunk.css"
+  },
+  {
+    "revision": "637ff445c366d4759c2bbdf68bc22140",
+    "url": "./index.html"
+  }
+];
