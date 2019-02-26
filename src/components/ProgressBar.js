5e5ecb78a486c4a7d1e1e321755d9bb03f2917c0
Carlos Sampol <sampol.90@gmail.com>
2019-02-26 16:41:52 -0500

    Fixing progress paint

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -5,8 +5,8 @@ const ProgressBar = ({ progress }) => (
   <div className="ProgressBar">
     <span>Staking round progress</span>
     <div className="ProgressBar-content">
-      <span className="ProgressBar-number">{progress}%</span>
-      <div className="ProgressBar-line" style={{ width: `${progress}%`}} />
+      <span className="ProgressBar-number">{progress > 100 ? 0 : progress}%</span>
+      <div className="ProgressBar-line" style={{ width: `${progress > 100 ? 0 : progress}%`}} />
     </div>
   </div>
 );
